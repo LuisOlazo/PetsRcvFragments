422233a9d700dadc3f0d3d33dbbd247547018742
Vanskarner <vanskarner@gmail.com>
2026-06-07 03:46:37 -0500

    The favorites activity was completed, the pet item design was updated, and the menu and RecyclerView were also included. The logic is still pending.

---
+++ app/src/main/java/com/luis/petsrcv/FavoriteActivities.java
@@ -0,0 +1,25 @@
+package com.luis.petsrcv;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FavoriteActivities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_favorites);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.favorites), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+}
